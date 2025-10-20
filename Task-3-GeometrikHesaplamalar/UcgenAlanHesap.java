3fa4950bd89632541cd6a68d3c4aa269f012da93
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-10-20 15:54:31 +0300

    Update UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,14 @@
+public class UcgenAlanHesap{
+
+    public static void main(String[] args) {
+       double a = 3;
+       double b = 4;
+       double c = 5;
+       double s = (a+b+c) / 2.0 ;
+       double alan = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+       System.out.println("Üçgenin Alanı : " + alan);
+
+    }
+
+}
