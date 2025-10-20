4ca9ee07111db617b8846a920dbe8449434b4283
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-10-20 16:37:52 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,10 @@
+public class MilKilometreTablosu{
+
+    public static void main(String[] args) {
+       int kilometre = 1;
+       double mil = kilometre * 1.609;
+       System.out.println("Mil :     1        5        10        20        50");
+       System.out.println("Km  :" +"   "+ mil+"    "+5*mil+ 5*kilometre+"   "+10*mil+10*kilometre+"   "+20*mil+20*kilometre+"   "+50*mil+50*kilometre);
+
+    }
+}
