b13c9618ccd20d8846b2137f443e506453f1a95b
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-10-20 17:02:53 +0300

    Update ParaBozma.java

---
+++ TASK-BONUS/ParaBozma.java
@@ -0,0 +1,7 @@
+public class ParaBozma{
+
+    public static void main(String[] args) {
+     int para = 278 ;
+     System.out.println(para + " TL= "+"1x200 , 1x50 , 1x20 , 1x5 , 1x2 , 1x1");
+    }
+}
