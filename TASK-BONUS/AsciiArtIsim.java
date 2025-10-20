9fc125ef3b2caa6892d834b2ae7e199a34bfe890
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-10-20 17:05:50 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,14 @@
+public class AsciiArtIsim{
+
+    public static void main(String[] args) {
+        
+        System.out.println("Y   Y      A      SSSSS      o      RRRR   ");
+        System.out.println(" Y Y      A A     S   S             R   R  ");
+        System.out.println("  Y      A   A    S          I      R   R  ");
+        System.out.println("  Y     AAAAAAA    SSSSS     I      RRRR   ");
+        System.out.println("  Y     A     A         S    I      R  R   ");
+        System.out.println("  Y     A     A     S   S    I      R   R  ");
+        System.out.println("  Y     A     A     SSSSS    I      R    R ");
+        
+    }
+}
