69b90273c3806147ffd784f0133b2cef9e951297
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-10-20 16:03:50 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+public class IsimKarti{
+
+    public static void main(String[] args) {
+       String ad = "Muhammet Yasir";
+       String soyad = "Çelik";
+       String ogrNo = "250542010";
+       String bolum = "Yazılım Mühendisliği";
+
+       System.out.println("--------------------------------------");
+       System.out.println("   ÖĞRENCİ BİLGİLERİ   ");
+       System.out.println("--------------------------------------");
+       System.out.println("Ad    : "+ad);
+       System.out.println("Soyad    : "+ soyad);
+       System.out.println("No    : "+ogrNo);
+       System.out.println("Bölüm    : "+bolum);
+       System.out.println("--------------------------------------");
+
+    }
+
+}
