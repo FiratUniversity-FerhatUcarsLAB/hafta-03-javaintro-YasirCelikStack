52abaef8fcc8187375537870bd5465773148b81c
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-10-20 15:46:29 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,14 @@
+public class DikdortgenHesap{
+
+    public static void main(String[] args) {
+       double dikdortgen_en = 4.5;
+       double dikdortgen_boy = 7.9 ;
+       double dikdortgen_alan = dikdortgen_boy*dikdortgen_en;
+       double dikdortgen_cevre = 2*dikdortgen_boy + 2*dikdortgen_en ;
+       System.out.println("Dikdörtgenin Eni : "+ dikdortgen_en + "  Dikdörtgenin Boyu : "+dikdortgen_boy);
+       System.out.println("Dikdörtgenin Alanı : "+dikdortgen_alan);
+       System.out.println("Dikdörtgenin Çevresi : "+dikdortgen_cevre);
+
+    }
+
+}
