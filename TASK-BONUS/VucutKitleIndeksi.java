41aa309aaae1fe60d07cbacd8ad4332faaa339af
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-10-20 17:00:36 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,10 @@
+public class VucutKitleIndeksi{
+
+    public static void main(String[] args) {
+      double boy = 1.81;
+      int kilo = 85 ;
+      double bmi = kilo / (boy*boy);
+      System.out.println(bmi);
+
+    }
+}
