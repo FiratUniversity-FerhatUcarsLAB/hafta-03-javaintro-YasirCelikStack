2396a288e0b0a21a693f72183a6b3a5e67fae6f5
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-10-20 14:45:43 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,12 @@
+public class BesKereTekrar{
+
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoşgeldiniz ! ");
+        System.out.println("Java'ya Hoşgeldiniz ! ");
+        System.out.println("Java'ya Hoşgeldiniz ! ");
+        System.out.println("Java'ya Hoşgeldiniz ! ");
+        System.out.println("Java'ya Hoşgeldiniz ! ");
+
+    }
+
+}
