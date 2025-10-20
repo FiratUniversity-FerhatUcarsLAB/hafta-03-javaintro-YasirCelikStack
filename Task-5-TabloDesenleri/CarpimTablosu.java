d5c3e09e41bc66c9dc762816678d361f65a4d40b
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-10-20 16:40:17 +0300

    Update CarpimTablosu.java

--- Task-5-TabloDesenleri/CarpimTablosu.java
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -1,1 +1,18 @@
+public class CarpimTablosu{
+
+    public static void main(String[] args) {
+       int sayi = 2;
+       System.out.println("2x1 = "+sayi*1);
+       System.out.println("2x2 = "+sayi*2);
+       System.out.println("2x3 = "+sayi*3);
+       System.out.println("2x4 = "+sayi*4);
+       System.out.println("2x5 = "+sayi*5);
+       System.out.println("2x6 = "+sayi*6);
+       System.out.println("2x7 = "+sayi*7);
+       System.out.println("2x8 = "+sayi*8);
+       System.out.println("2x9 = "+sayi*9);
+       System.out.println("2x10 = "+sayi*10);
+
+    }
+}
 
