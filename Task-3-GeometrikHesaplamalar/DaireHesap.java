6564b4de1eb37dd6905397d5e4a8ca78ce64f87e
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-10-20 15:42:16 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,14 @@
+public class DaireHesap{
+
+    public static void main(String[] args) {
+       double yaricap = 5.5 ;
+       double pi = 3.14159;
+       double dairecevre = pi * 2 * yaricap;
+       double dairealan = pi * yaricap * yaricap;
+       System.out.println("Yarıçap : "+yaricap);
+       System.out.println("Yarıçapı 5.5 Olan Dairenin Çevresi : " + dairecevre);
+       System.out.println("Yarıçapı 5.5 Olan Dairenin Alanı : " + dairealan);
+
+    }
+
+}
